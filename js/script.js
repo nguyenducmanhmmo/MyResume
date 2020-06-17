f5174879f3dcfa839749c30ba235dbf970b47484
Manh <nguyenducmanhmmo@gmail.com>
2020-06-17 23:22:03 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
-$(function () {
-  $('a[href*="#"]:not([href="#"])').click(function () {
-    if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-      if (target.length) {
-        $("html, body").animate(
-          {
-            scrollTop: target.offset().top,
-          },
-          1000
-        );
-        return false;
-      }
+$(document).ready(function () {
+  // Transition effect for navbar and back-to-top icon
+  $(window).scroll(function () {
+    // checks if window is scrolled more than 500px, adds/removes solid class
+    if ($(this).scrollTop() > 550) {
+      $(".navbar").addClass("solid");
+    } else {
+      $(".navbar").removeClass("solid");
     }
+  });
+
+  // Scrolling effect for Arrow icons
+  $("#scrollIcon").click(function (e) {
+    e.preventDefault();
+    $.scrollTo($("#about"), 1000);
+  });
+  $("#nav-about").click(function (e) {
+    e.preventDefault();
+    $.scrollTo($("#about"), 1000);
+  });
+  $("#nav-portfolio").click(function (e) {
+    e.preventDefault();
+    $.scrollTo($("#portfolio"), 1000);
+  });
+  $("#nav-contact").click(function (e) {
+    e.preventDefault();
+    $.scrollTo($("#contact"), 1000);
+  });
+  $(".navbar-brand").click(function (e) {
+    e.preventDefault();
+    $.scrollTo(0, 1000);
   });
 });
